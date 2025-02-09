41ceb1c830769d75d315801163c4ef2e4e7f586c
GolovaStudio <frost-44@ya.ru>
2025-02-09 14:50:50 +0300

    Lesson 4  - sockets #2

--- chatper_04/4.2.1_aiohttp_requests.py
+++ chatper_04/4.2.1_aiohttp_requests.py
@@ -7,14 +7,14 @@ from util import async_timed
 @async_timed()
 async def fetch_status(session: ClientSession, url: str) -> int:
     # задать тайм-аут
-    ten_millis = aiohttp.ClientTimeout(total=.1)
+    ten_millis = aiohttp.ClientTimeout(total=.9)
     async with session.get(url, timeout=ten_millis) as result:
         return result.status
 
 @async_timed()
 async def main():
     # сессионный тайм-аут
-    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+    session_timeout = aiohttp.ClientTimeout(total=1, connect=.8)
     async with aiohttp.ClientSession(timeout=session_timeout) as session:
         url = 'https://www.example.com'
         status = await fetch_status(session, url)
--- chatper_04/4.4_asyncio.gather.py
+++ chatper_04/4.4_asyncio.gather.py
@@ -1,7 +1,7 @@
 import asyncio
 import aiohttp
 from aiohttp import ClientSession
-from chater_04 import fetch_status
+from chatper_04 import fetch_status
 from util import async_timed
 
 
@@ -9,7 +9,7 @@ from util import async_timed
 async def main():
     session_timeout = aiohttp.ClientTimeout(total=3, connect=.5)
     async with ClientSession(timeout=session_timeout) as session:
-        urls = ['https://www.example.com/' for _ in range(1000)]
+        urls = ['https://www.example.com/' for _ in range(10)]
         # в requests попадут 1000 корутин
         requests = [fetch_status(session, url) for url in urls]
         # а теперь выполним конкурентно все задачи/корутины
